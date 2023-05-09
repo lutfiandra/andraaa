435311736b9d5bfa54ec4cf1c8284fa5f8b857e2
lutfiandra <129819043+lutfiandra@users.noreply.github.com>
2023-05-09 11:31:24 +0700

    Add files via upload

---
+++ Latihan 1per6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int pil;
+    char back;
+    int a = 10;
+    int b = 5;
+    int c;
+
+    do{
+        cout<<"0.exit"<<endl
+        cout<<"1.penjumlahan"endl;
+        cout<<"pilih 1"<<endl
+        cin>>(pil){
+        switch(pil){
+            case 0;
+                return 0;
+            case 1:
+                    cout<<"exit"<<endl;
+        }
+    }
+}
---
+++ Latihan2per6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     int a = 10;
+     int b = 5;
+     int c, pil;
+
+     pilihan:
+        cout<<"0.exit"<<endl;
+        cout<<"1.penjumlahan"
+        cin>>"pilihan : "
+        cin>>pil;
+
+    if(pil ==0){
+        cout<<"thanks"<<endl;
+        return 0;
+    }else if(pil==1){
+        c=a+b;
+        cout<<c<<endl;
+        goto pilihan;
+    }else{
+        cout<<"pilihan salah"<<endl;
+        goto pilihan;
+    }
+
+    return 0;
+
+}
+   
